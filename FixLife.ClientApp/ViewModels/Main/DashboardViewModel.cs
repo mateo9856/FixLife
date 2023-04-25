089a1b5b3413d8b110fe691e6f38454179228634
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2023-04-25 22:04:34 +0200

    init dashboard ctor

--- FixLife.ClientApp/ViewModels/Main/DashboardViewModel.cs
+++ FixLife.ClientApp/ViewModels/Main/DashboardViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace FixLife.ClientApp.ViewModels.Main
 {
     public class DashboardViewModel : BaseViewModel
     {
+        public ICommand EditPlanCommand { get; private set; }
+
+        public DashboardViewModel()
+        {
+            EditPlanCommand = new Command(async e => await EditPlan());
+        }
+
+        private async Task EditPlan()
+        {
+            
+        }
+
     }
 }
