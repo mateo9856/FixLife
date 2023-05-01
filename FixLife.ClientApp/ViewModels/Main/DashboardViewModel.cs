fe0465faefd0f046c834ca0bf4438316bcd9ff94
mateo9856 <mateo9656@gmail.com>
2023-05-01 21:21:58 +0200

    dashboard part 1

--- FixLife.ClientApp/ViewModels/Main/DashboardViewModel.cs
+++ FixLife.ClientApp/ViewModels/Main/DashboardViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FixLife.ClientApp.Models.AppPlan;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,32 @@ namespace FixLife.ClientApp.ViewModels.Main
     public class DashboardViewModel : BaseViewModel
     {
         public ICommand EditPlanCommand { get; private set; }
+        public AppPlan ActualPlan { get; set; }
+        
+        public string WorkStatus { 
+            get 
+            {
+                if (ActualPlan == null) return string.Empty;
+                return InWorkState() ? "In work" : "After Work";
+            } 
+        }
+
+        public bool TimesLeftVisible { get => InWorkState(); }
+        public bool TimeToWorkVisible { get => !InWorkState(); }
 
         public DashboardViewModel()
         {
+            ActualPlan = new AppPlan();//TODO: Get Data from Api
             EditPlanCommand = new Command(async e => await EditPlan());
+        }
+
+        private bool InWorkState()
+        {
+            if(ActualPlan == null) return false;
+            var nowDate = DateTime.Parse(DateTime.Now.ToString());
+            var startDate = DateTime.Parse(ActualPlan.WeeklyWork.TimeStart.ToString());
+            var endDate = DateTime.Parse(ActualPlan.WeeklyWork.TimeEnd.ToString());
+            return nowDate > startDate && nowDate < endDate;        
         }
 
         private async Task EditPlan()
