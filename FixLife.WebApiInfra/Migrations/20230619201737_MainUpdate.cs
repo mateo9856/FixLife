a135188e7c3e9627a2abce2017db720dd7fcaf82
mateo9856 <mateo9656@gmail.com>
2023-06-19 22:59:55 +0200

    Migration Update

---
+++ FixLife.WebApiInfra/Migrations/20230619201737_MainUpdate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FixLife.WebApiInfra.Migrations
+{
+    /// <inheritdoc />
+    public partial class MainUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DayOfWeeks",
+                table: "WeeklyWorks");
+
+            migrationBuilder.CreateTable(
+                name: "DayOfWeek",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Day = table.Column<int>(type: "int", nullable: false),
+                    WeeklyWorkId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DayOfWeek", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DayOfWeek_WeeklyWorks_WeeklyWorkId",
+                        column: x => x.WeeklyWorkId,
+                        principalTable: "WeeklyWorks",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DayOfWeek_WeeklyWorkId",
+                table: "DayOfWeek",
+                column: "WeeklyWorkId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DayOfWeek");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DayOfWeeks",
+                table: "WeeklyWorks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
