0748a8b87e749482d8992b92fa551ad324e569b4
mateo9856 <mateo9656@gmail.com>
2023-06-24 13:03:12 +0200

    Create User Not found exception

---
+++ FixLife.WebApiDomain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixLife.WebApiDomain.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException()
+        {
+        }
+
+        public UserNotFoundException(string message)
+            : base(message)
+        {
+        }
+    }
+}
