4e3bee92081b4b7eb5e4cae641b26717205e16f8
mateo9856 <mateo9656@gmail.com>
2023-08-02 20:23:32 +0200

    implement timer notifaction class

---
+++ FixLife.ClientApp/Common/NotificationSender.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using Plugin.LocalNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixLife.ClientApp.Common
+{
+    public class NotificationSender
+    {
+        public void SendWindowsToast(string header, string message)
+        {
+            #if WINDOWS
+            var builder = new ToastContentBuilder();
+            builder.AddText(header);
+            builder.AddText(message);
+            builder.Show();
+            #endif
+        }
+        public void SendMobileToast(string header, string message) {
+            var request = new NotificationRequest();
+            request.NotificationId = 999;
+            request.Title= header;
+            request.Subtitle = "Life notification";
+            request.Description = message;
+            LocalNotificationCenter.Current.Show(request);
+        }
+    }
+}
---
+++ FixLife.ClientApp/Common/NotificationTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace FixLife.ClientApp.Common
+{
+    public static class NotificationTimer
+    {
+        private static System.Timers.Timer _timer;
+
+        static NotificationTimer()
+        {
+            _timer = new System.Timers.Timer(new TimeSpan(0, 0, 15, 0));
+            _timer.Elapsed += OnTiming;
+        }
+
+        private static void OnTiming(object sender, ElapsedEventArgs e)
+        {
+
+        }
+    }
+}
