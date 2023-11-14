0c9416959a33d8880b259f333472f4660f100b6c
mateo9856 <mateo9656@gmail.com>
2023-11-14 22:21:30 +0100

    Create test topic check

--- FixLife.ClientApp/Infrastructure/MessageBroker/CreatePlanKafkaProducer.cs
+++ FixLife.ClientApp/Infrastructure/MessageBroker/CreatePlanKafkaProducer.cs
@@ -1,10 +1,12 @@
 ﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 
 namespace FixLife.ClientApp.Infrastructure.MessageBroker
 {
     public class CreatePlanKafkaProducer : IDisposable
     {
         private ProducerConfig _config;
+        private const string TopicName = "FixLife-CreatePlanLogs";
 
         public CreatePlanKafkaProducer()
         {
@@ -20,18 +22,46 @@ namespace FixLife.ClientApp.Infrastructure.MessageBroker
         {
             try
             {
+                await ValidateOrCreateTopic(TopicName);
                 using var producer = new ProducerBuilder<string, string>(_config).Build();
                 var message = new Message<string, string>()
                 {
                     Key = "Plan create",
                     Value = string.Format("Value created by {0}, created plans: {1}", user, plans)
                 };
-                var deliver = await producer.ProduceAsync("FixLife-CreatePlanLogs", message);
+                var deliver = await producer.ProduceAsync(TopicName, message);
 
             } catch (Exception ex)
             {
                 return;
             }
+        }
+
+        private async Task ValidateOrCreateTopic(string topic)
+        {
+            var adminConfig = new AdminClientConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            using var adminClient = new AdminClientBuilder(adminConfig).Build();
+            var metadata = adminClient.GetMetadata(TimeSpan.FromSeconds(10));
+            var isHaveTopic = metadata.Topics.Any(d => d.Topic.Equals(topic));
+            
+            if (isHaveTopic) {
+                return;
+            }
+
+            var newTopic = new TopicSpecification
+            {
+                Name = TopicName,
+                NumPartitions = 1,
+                ReplicationFactor = 1,
+            };
+
+            await adminClient.CreateTopicsAsync(new List<TopicSpecification>
+            {
+                newTopic,
+            });
         }
 
         public void Dispose()
