80c537b2e9f4bab21d165341ac40ada3d03b947d
Mateusz Magdziak <51763969+mateo9856@users.noreply.github.com>
2024-01-23 21:07:34 +0100

    2110-rewrite to own ConsumerBuilder with DI

--- FixLife.KafkaLog/Program.cs
+++ FixLife.KafkaLog/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Kfk = FixLife.Kafka;
 
 namespace FixLife.KafkaLog
 {
@@ -6,33 +8,29 @@ namespace FixLife.KafkaLog
     {
         static void Main(string[] args)
         {
+            var serviceProvider = new ServiceCollection()
+            .AddSingleton<Kfk.Interfaces.IConsumer<Ignore, string>, Kfk.Services.ConsumerService<Ignore, string>>()
+            .BuildServiceProvider();
+
             System.Timers.Timer timer = new System.Timers.Timer(20000);
             timer.Enabled = true;
             timer.Elapsed += WriteCloseInfo;
 
-            var config = new ConsumerConfig
-            {
-                BootstrapServers = "localhost:9092",
-                ClientId = "FixLife",
-                GroupId = "default",
-                BrokerAddressFamily = BrokerAddressFamily.V4,
-                EnablePartitionEof = true,
-            };
+            var consumerService = serviceProvider
+            .GetService<Kfk.Interfaces.IConsumer<Ignore, string>>();
 
-            using var consumer = new ConsumerBuilder<Ignore, string>(config)
-                .SetErrorHandler(((_, e) => Console.WriteLine($"Detected error ! : Code : {e.Code}\n Reason: {e.Reason}")))
-                .Build();
-            consumer.Subscribe("FixLife-CreatePlanLogs");
+            var consumer = new ConsumerBuilder(consumerService);
+            consumer.StartProduce();
 
             string key = "";
 
             while(key == Console.ReadLine())
             {
-                var consumeResult = consumer.Consume();
+                var consumeResult = consumer.ConsumeValue();
                 Console.WriteLine(consumeResult.Value);
             }
 
-            consumer.Close();
+            consumer.StopProduce();
         }
 
         private static void WriteCloseInfo(object? sender, System.Timers.ElapsedEventArgs e)
