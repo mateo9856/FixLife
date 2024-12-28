b2f058cee4aef45fe5125de72f8975e1327c5145
mateo9856 <mateo9656@gmail.com>
2024-12-28 01:31:07 +0100

    client app mapping fix #1

--- FixLife.ClientApp/Models/AppPlan/FreeTime.cs
+++ FixLife.ClientApp/Models/AppPlan/FreeTime.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FixLife.ClientApp.Models
+﻿namespace FixLife.ClientApp.Models
 {
     public class FreeTime
     {
